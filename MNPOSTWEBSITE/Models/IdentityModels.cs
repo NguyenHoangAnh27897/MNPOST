bb57f56fb98836412e2d3dc96ac5504abef6cd41
NguyenHoangAnh27897 <hoanganh27897@gmail.com>
2018-06-28 14:54:14 +0700

    Update MNPOSTCOMMON add Model

--- MNPOSTWEBSITE/Models/IdentityModels.cs
+++ MNPOSTWEBSITE/Models/IdentityModels.cs
@@ -5,7 +5,9 @@ namespace MNPOSTWEBSITE.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
-
+        public string IsActive { get; set; }
+        public string FullName { get; set; }
+        public string Phone { get; set; }
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
