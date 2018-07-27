d549027d5db97262910281e811c71512e1321c13
LE HOAI NAM <lenam.leo@gmail.com>
2018-07-28 04:22:07 +0700

    nam

--- MNPOST/Controllers/mailer/MailerInitController.cs
+++ MNPOST/Controllers/mailer/MailerInitController.cs
@@ -2,14 +2,72 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using MNPOST.Models;
 using System.Web.Mvc;
 
 namespace MNPOST.Controllers.mailer
 {
     public class MailerInitController : MailerController
     {
-        
 
+        [HttpGet]
+        public ActionResult Init()
+        {
+            var allCus = db.BS_Customers.ToList();
+
+            List<CustomerInfoResult> cusResult = new List<CustomerInfoResult>();
+
+            foreach (var item in allCus)
+            {
+                cusResult.Add(new CustomerInfoResult()
+                {
+                    code = item.CustomerCode,
+                    name = item.CustomerName,
+                    phone = item.Phone,
+                    provinceId = item.ProvinceID,
+                    address = item.Address,
+                    districtId = item.DistrictID,
+                    wardId = item.WardID
+                });
+            }
+
+            ViewBag.Customers = cusResult;
+
+            //
+            List<CommonData> allMailerType = new List<CommonData>();
+            allMailerType.Add(new CommonData()
+            {
+                code = "CPN",
+                name = "Chuyển phát nhanh"
+            });
+            allMailerType.Add(new CommonData()
+            {
+                code = "CPT",
+                name = "Chuyển phát thường"
+            });
+
+
+            ViewBag.MailerTypes = allMailerType;
+
+            //
+            List<CommonData> allPayment = new List<CommonData>();
+
+            allPayment.Add(new CommonData()
+            {
+                code = "SENDERPAY",
+                name = "Người gửi thanh toán"
+            });
+
+            allPayment.Add(new CommonData()
+            {
+                code = "RECEIVERPAY",
+                name = "Người nhận thanh toán"
+            });
+
+            ViewBag.Payments = allPayment;
+
+            return View();
+        }
 
 
 	}
---
+++ MNPOST/Scripts/mydirective.js
@@ -0,0 +1,16 @@
+﻿var app = angular.module('myDirective', []);
+
+
+app.directive('convertToNumber', function () {
+    return {
+        require: 'ngModel',
+        link: function(scope, element, attrs, ngModel) {
+            ngModel.$parsers.push(function(val) {
+                return parseInt(val, 10);
+            });
+            ngModel.$formatters.push(function(val) {
+                return '' + val;
+            });
+        }
+    };
+});
