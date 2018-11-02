013959679b0b389fd933fd15c68042f09dd04d97
Hoang Anh <hoananh27897@gmail.com>
2018-11-02 11:22:40 +0700

    Update MNPOSTWEBSITE

---
+++ MNPOSTWEBSITE/Controllers/CustomerController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MNPOSTWEBSITEMODEL;
+
+namespace MNPOSTWEBSITE.Controllers
+{
+    public class CustomerController : ApiController
+    {
+    }
+}
